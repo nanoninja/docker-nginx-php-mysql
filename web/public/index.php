ca9f2919aa7403043a755ad1a114af61729a8a85
Vincent Letourneau <vincent@nanoninja.com>
2017-01-23 11:03:50 +0100

    Add example

--- web/public/index.php
+++ web/public/index.php
@@ -1,10 +1,15 @@
-<!DOCTYPE html>
+<?php
+
+include '../app/vendor/autoload.php';
+$foo = new App\Acme\Foo();
+
+?><!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8">
-        <title>Docker Nginx PHP MySQL</title>
+        <title>Docker <?php echo $foo->getName(); ?></title>
     </head>
     <body>
-        <h1>Docker Nginx PHP MySQL</h1>
+        <h1>Docker <?php echo $foo->getName(); ?></h1>
     </body>
 </html>
