97ef028732d8b46e0f1b803db952949046a650bf
Vincent Letourneau <vincent@nanoninja.com>
2017-06-13 22:18:16 +0200

    Improve linux cleaner

--- bin/linux/clean.sh
+++ bin/linux/clean.sh
@@ -1,14 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 ROOT_PATH=$1
 
 if [[ ! -d "$ROOT_PATH" && ! -L "$ROOT_PATH" ]]; then
     echo "No such file or directory"
+    exit 1
 fi
 
-WEB_PATH=$ROOT_PATH/web
 DATA_PATH=$ROOT_PATH/data
 ETC_PATH=$ROOT_PATH/etc
+WEB_PATH=$ROOT_PATH/web
 APP_PATH=$WEB_PATH/app
 
 rm -Rf $DATA_PATH/db/mysql/*
@@ -16,7 +17,7 @@ rm -Rf $DATA_PATH/dumps/*
 rm -Rf $APP_PATH/vendor
 rm -Rf $APP_PATH/composer.lock
 
-# remove exited containers:
+# remove exited containers
 docker rm $(docker ps -a -f status=exited -q)
 
 docker volume rm $(docker volume ls -qf dangling=true)
