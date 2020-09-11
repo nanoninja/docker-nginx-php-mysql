1721fead886d802f04d390d49c070348b4bd620b
Nikolay Stankov <nstankov@iccsafe.org>
2020-09-11 16:05:13 +0000

    tets

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 docker stack deploy --compose-file ../docker-swarm.yml  lnch-n-lrn
-docker service update --config-add source=nginx_default,target=/etc/nginx/conf.d/default.conf,mode=0440 web
-docker service update --config-add source=nginx_default_template,target=/etc/nginx/conf.d/default.template,mode=0440 web
-docker service update --config-add source=php_ini,target=/usr/local/etc/php/conf.d/php.ini,mode=0440 php
+docker service update --config-add source=nginx_default,target=/etc/nginx/conf.d/default.conf,mode=0440 lnch-n-lrn_web
+docker service update --config-add source=nginx_default_template,target=/etc/nginx/conf.d/default.template,mode=0440 lnch-n-lrn_web
+docker service update --config-add source=php_ini,target=/usr/local/etc/php/conf.d/php.ini,mode=0440 lnch-n-lrn_php
