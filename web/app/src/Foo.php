2e6ab12c3d4ffc3b9bbe1bef5d5d38baff5a8710
Vincent Letourneau <vincent@nanoninja.com>
2020-09-24 13:31:51 +0200

    Add some documentations for phpdoc

--- web/app/src/Foo.php
+++ web/app/src/Foo.php
@@ -1,9 +1,19 @@
 <?php
 
+/**
+ * I belong to a file
+ */
+
 namespace App\Acme;
 
+/**
+ * I belong to a class
+ */
 class Foo
 {
+    /**
+     * Gets the name of the application.
+     */
     public function getName()
     {
         return 'Nginx PHP MySQL';
